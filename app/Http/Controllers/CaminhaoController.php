37bf9497e11b92dd4e01512d7d0cb710c3372c90
VitorMouraV <103595412+VitorMouraV@users.noreply.github.com>
2022-10-30 20:22:24 -0300

    first commit

---
+++ app/Http/Controllers/CaminhaoController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Http\Request;
+use App\Models\Caminhao;
+
+class CaminhaoController extends Controller
+{
+    public function FormularioCadastro(){
+        return view('cadastrarCaminhao');
+    }
+
+    public function MostrarEditarCaminhao(Request $request){
+
+       // $dadosCaminhao = Caminhao::all();
+        //dd($dadosCaminhao);
+        $dadosCaminhao = Caminhao::query();
+        $dadosCaminhao->when($request->marca,function($query, $vl){
+            $query->where('marca','like','%'.$vl.'%');
+        });
+
+        $dadosCaminhao = $dadosCaminhao->get();
+
+
+        return view('editarCaminhao',[
+            'registrosCaminhao' => $dadosCaminhao
+        ]);
+        
+    }
+//---------------------------------------------------------------------------------------------------SALVAR BANCO-------------------------------------------------------------------------
+    public function SalvarBanco(Request $request){
+
+        $dadosCaminhao = $request->validate([
+            'modelo' => 'string|required',
+            'marca' => 'string|required',
+            'ano' =>'string|required',
+            'cor' => 'string|required',
+            'valor' => 'string|required'
+        ]);
+
+        Caminhao::create($dadosCaminhao);
+        return Redirect::route('home');
+    }
+//---------------------------------------------------------------------------------------------------------APAGAR----------------------------------------------------------------------------------------
+    public function ApagarBancoCaminhao(Caminhao $registrosCaminhoes){
+        //dd($registrosCaminhoes);
+        $registrosCaminhoes->delete();
+        //Caminhao::findOrFail($id)->delete();
+        return Redirect::route('editar-caminhao');
+    }
+
+    public function MostrarAlterarCaminhao(Caminhao $registrosCaminhoes){
+        return view('alterarCaminhao',['registrosCaminhoes' => $registrosCaminhoes]);
+    }
+    public function AlterarBancoCaminhao(Caminhao $registrosCaminhoes, Request $request){
+        $banco = $request->validate([
+            'modelo' => 'string|required',
+            'marca' => 'string|required',
+            'ano' =>'string|required',
+            'cor' => 'string|required',
+            'valor' => 'string|required'
+        ]);
+
+        $registrosCaminhoes->fill($banco);
+        $registrosCaminhoes->save();
+
+        return Redirect::route('editar-caminhao');
+
+
+    }
+
+}
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CaminhaoController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+
+Route::get('/', function () {
+    return view('welcome');
+});
+*/
+
+Route::get('/',[HomeController::class,'MostrarHome'])->name('home');
+Route::get('/editar-caminhao',[CaminhaoController::class,'MostrarEditarCaminhao'])->name('editar-caminhao');
+Route::get('/cadastrar-caminhao',[CaminhaoController::class,'FormularioCadastro'])->name('cadastrar-caminhao');
+Route::post('/cadastrar-caminhao',[CaminhaoController::class,'SalvarBanco'])->name('salvar-banco');
+//deletar
+
+Route::delete('/editar-caminhao/{registrosCaminhoes}',[CaminhaoController::class,'ApagarBancoCaminhao'])->name('apagar-caminhao');
+//alterar caminhao
+Route::get('/alterar-caminhao/{registrosCaminhoes}',[CaminhaoController::class,'MostrarAlterarCaminhao'])->name('alterar-caminhao');
+Route::put('/editar-caminhao/{registrosCaminhoes}',[CaminhaoController::class,'AlterarBancoCaminhao'])->name('alterar-banco-caminhao');
